1f7a8685e21c52bc1f3f207803c74f42fee3a05e
ItsMinhaj <minhajul.islam021@gmail.com>
2022-06-05 23:44:08 +0600

    Show favorite items

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:provier_master_series/provider/add_to_fav_provider.dart';
 import 'package:provier_master_series/provider/slider_opacity_provider.dart';
 import 'package:provier_master_series/screen/add_to_favorite/add_to_favorite.dart';
-import 'package:provier_master_series/screen/slider_opacity.dart';
 
 void main() {
   runApp(const MyApp());
